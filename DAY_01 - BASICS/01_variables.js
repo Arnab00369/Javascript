85dc338a6ae56b09f880c12e0a03148a7bd4e92a
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-12 04:43:31 +0000

    Variable_name_change

---
+++ DAY_01 - BASICS/01_variables.js
@@ -0,0 +1,26 @@
+//varibales' names should always be in Camel Case
+
+//NOTE:: Never use 'var', now-a-days due to block and functional scope issues and values changes
+//Use only 'let' and 'const' for variables (V.V.Important)
+
+const accountId = 458168
+let accountEmail = "arnabjana123456@gmail.com"
+var accountPassword = "123456"
+accountCity = "Kolkata"
+let accountState; 
+// For 'let' if no value is given in the variable then it is undefined
+
+//accountId = 2 
+// For 'const' or constant variable you cannot change values
+
+// Changing and checking if changes and modifications are allowed or not
+
+/*accountEmail = "aabjna123456@gmail.com"
+accountPassword = "1456"
+accountCity = "Kolta"*/
+
+//For above variable types like 'let', 'var' and normal variables we can modify
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
