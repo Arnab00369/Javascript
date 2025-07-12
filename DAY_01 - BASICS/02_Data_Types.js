1be747f088b815c3b8065044ca2924fc2e785600
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-12 05:28:08 +0000

    Data_Types_done

--- DAY_01 - BASICS/02_Data_Types.js
+++ DAY_01 - BASICS/02_Data_Types.js
@@ -5,3 +5,30 @@
 //Increasinf readability bu using ; and using different lines for variables
 console.log(3+3);
 console.log("Arnab Jana");
+
+// Data Types::
+
+let name = "Arnab Jana"; // String
+let age = 21; // Number
+let isAdult = true; //boolean
+
+/*Types::
+==> Primitive Data Types::
+    ====================
+    1) Numbers ==> Maximum range = 2 ^ 53
+    2) bigint ==> Used for larger integer or number values, generally for e.g. : Trading, Stock-Market 
+    3) String ==> 'Single Quote' ; "Double Quotes"
+    4) Boolean ==> True/False
+    5) null ==> Standalone Value / Empty Value
+    6) undefined ==> Variable is decalered but value not given
+    7) Symbol ==> Generally, used for unique-ness, in React JS, and so on
+
+==> Non-Primitive Data Types::
+    1) Object
+*/
+
+// typeof is used to determine the Data Type of any variable
+console.log(typeof "Arnab Jana"); 
+console.log(typeof age);
+console.log(typeof null);  //Ouptut : Data Type of NULL is = Object (V.V.Important)
+console.log(typeof undefined); //Output : Data Type = undefined
