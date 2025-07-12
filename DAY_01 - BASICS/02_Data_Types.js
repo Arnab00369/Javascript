0a3e50afffe8bc0d4137e5126d0213c9af894f61
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-12 04:51:38 +0000

    Data_Types_changes

---
+++ DAY_01 - BASICS/02_Data_Types.js
@@ -0,0 +1,7 @@
+"use strict"; // Treating all JS codes as newer version
+
+//alert("Hello");// This syntax is only valid and can be executed on console not on nodejs
+
+//Increasinf readability bu using ; and using different lines for variables
+console.log(3+3);
+console.log("Arnab Jana");
